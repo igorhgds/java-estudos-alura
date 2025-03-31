68c9c11b3eb7780eebd5713a6144b2963cbfdb2f
igorhgds <igorhenrique.gomes@hotmail.com>
2025-03-31 14:54:08 -0300

    poo - minhasMusicas

--- java-poo/src/exercicios/minhasMusicas/principal/Principal.java
+++ java-poo/src/exercicios/minhasMusicas/principal/Principal.java
@@ -1,4 +1,40 @@
 package exercicios.minhasMusicas.principal;
 
+import exercicios.minhasMusicas.modelos.MinhasPreferidas;
+import exercicios.minhasMusicas.modelos.Musica;
+import exercicios.minhasMusicas.modelos.PodCast;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Walk");
+        minhaMusica.setArtista("Foo Fighters");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        PodCast podCast = new PodCast();
+        podCast.setTitulo("Inglês do Zero ao Cem");
+        podCast.setApresentador("Teacher Jay");
+        podCast.setDescricao("Curso de inglês em formato de podcast");
+
+        for (int i = 0; i < 5000; i++) {
+            podCast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            podCast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(podCast);
+        preferidas.inclui(minhaMusica);
+
+    }
 }
