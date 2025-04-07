79b329d1a6cab65e3bf8145759791fe0b24712a7
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-07 10:41:42 -0300

    validação com Bean Validation

--- spring-boot/spring-boot3/src/main/java/igor/henrique/api/controller/MedicoController.java
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import igor.henrique.api.dto.CadastroMedicoDTO;
 import igor.henrique.api.entity.Endereco;
 import igor.henrique.api.entity.Medico;
 import igor.henrique.api.repository.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody CadastroMedicoDTO dados){
+    public void cadastrar(@RequestBody @Valid CadastroMedicoDTO dados){
         repository.save(new Medico(dados));
     }
 }
