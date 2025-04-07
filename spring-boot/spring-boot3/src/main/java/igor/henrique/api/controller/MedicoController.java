47e29a9f69be9ce99404c6b24272266d26662208
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-07 12:59:38 -0300

    ordenacao

--- spring-boot/spring-boot3/src/main/java/igor/henrique/api/controller/MedicoController.java
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +31,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<ListagemMedicosDTO> listar(Pageable paginacao){
+    public Page<ListagemMedicosDTO> listar(@PageableDefault(size = 5, sort = "nome") Pageable paginacao){
         return repository.findAll(paginacao).map(ListagemMedicosDTO::new);
     }
 }
