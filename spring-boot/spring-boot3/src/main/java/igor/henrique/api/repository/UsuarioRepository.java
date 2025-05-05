1d50ba35c5fbce7e920239c08ffcc7706784b4bd
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-05 12:17:04 -0300

    criado AutenticacaoService e UsuarioRepository

---
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package igor.henrique.api.repository;
+
+import igor.henrique.api.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+}
---
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/service/auth/AutenticacaoService.java
@@ -0,0 +1,20 @@
+package igor.henrique.api.service.auth;
+
+import igor.henrique.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
