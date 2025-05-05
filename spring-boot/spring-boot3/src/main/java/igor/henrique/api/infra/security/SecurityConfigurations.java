9a384c1779e931ae0a2b8782c5bcd592b29f1c1e
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-05 13:08:17 -0300

    gerando autenicacao usuario e informando o tipo de hashcode

--- spring-boot/spring-boot3/src/main/java/igor/henrique/api/infra/security/SecurityConfigurations.java
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/infra/security/SecurityConfigurations.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,4 +29,8 @@ public class SecurityConfigurations {
         return configuration.getAuthenticationManager();
     }
 
+    @Bean //informando o tipo de codigo utilizado, nesse caso o BCrypt.
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
