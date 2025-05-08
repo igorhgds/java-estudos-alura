9401fed0a5c2c8f7db4ddad6dba91a51c858bffb
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-08 09:49:46 -0300

    criando TokenServic e realizado teste de login

--- spring-boot/spring-boot3/src/main/java/igor/henrique/api/controller/AutenticacaoController.java
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package igor.henrique.api.controller;
 
 import igor.henrique.api.dto.usuario.DadosAutenticacaoDTO;
+import igor.henrique.api.entity.Usuario;
+import igor.henrique.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
