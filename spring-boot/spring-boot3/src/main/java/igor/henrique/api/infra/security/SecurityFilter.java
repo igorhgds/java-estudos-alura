f2c44d7988b3528f8dbf3dea101d87ad7d33e8eb
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-08 12:23:40 -0300

    recuperando token do cabeçalho

--- spring-boot/spring-boot3/src/main/java/igor/henrique/api/infra/security/SecurityFilter.java
+++ spring-boot/spring-boot3/src/main/java/igor/henrique/api/infra/security/SecurityFilter.java
@@ -14,8 +14,18 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+
 
         filterChain.doFilter(request, response); //habilita chamar o próximo filtro
 
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
