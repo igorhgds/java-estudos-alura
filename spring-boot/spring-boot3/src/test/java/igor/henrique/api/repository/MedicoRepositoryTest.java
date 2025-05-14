498b6277ef0d388b193f5c0f14dc6bcaf9cfb3e8
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-14 12:26:49 -0300

    testando o repository

--- spring-boot/spring-boot3/src/test/java/igor/henrique/api/repository/MedicoRepositoryTest.java
+++ spring-boot/spring-boot3/src/test/java/igor/henrique/api/repository/MedicoRepositoryTest.java
@@ -1,18 +1,120 @@
 package igor.henrique.api.repository;
 
+import igor.henrique.api.dto.DadosEnderecoDTO;
+import igor.henrique.api.dto.medico.CadastroMedicoDTO;
+import igor.henrique.api.dto.paciente.CadastroPacienteDTO;
+import igor.henrique.api.entity.Consulta;
+import igor.henrique.api.entity.Medico;
+import igor.henrique.api.entity.Paciente;
+import igor.henrique.api.enums.Especialidade;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private EntityManager em;
+
     @Test
-    void escolherMedicoAleatorioLivreNaData() {
+    @DisplayName("Deveria devolver null quando unico medico cadastrado nao esta disponivel na data")
+    void escolherMedicoAleatorioLivreNaDataCenario1() {
+        // given ou arrange
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.CARDIOLOGIA);
+        var paciente = cadastrarPaciente("Paciente", "paciente@email.com", "00011122234");
+        cadastrarConsulta(medico, paciente, proximaSegundaAs10);
+
+        //then ou act
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA, proximaSegundaAs10);
+
+        //then ou assert
+        assertThat(medicoLivre).isNull();
+
+    }
+
+    @Test
+    @DisplayName("Deveria devolver medico quando ele estiver disponivel na data")
+    void escolherMedicoAleatorioLivreNaDataCenario2() {
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+
+        var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.CARDIOLOGIA);
+
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA, proximaSegundaAs10);
+        assertThat(medicoLivre).isEqualTo(medico);
+
+    }
+
+
+    private void cadastrarConsulta(Medico medico, Paciente paciente, LocalDateTime data) {
+        em.persist(new Consulta(null, medico, paciente, data, null));
+    }
+
+    private Medico cadastrarMedico(String nome, String email, String crm, Especialidade especialidade) {
+        var medico = new Medico(dadosMedico(nome, email, crm, especialidade));
+        em.persist(medico);
+        return medico;
+    }
+
+    private Paciente cadastrarPaciente(String nome, String email, String cpf) {
+        var paciente = new Paciente(dadosPaciente(nome, email, cpf));
+        em.persist(paciente);
+        return paciente;
+    }
+
+    private CadastroMedicoDTO dadosMedico(String nome, String email, String crm, Especialidade especialidade) {
+        return new CadastroMedicoDTO(
+                nome,
+                email,
+                "61999999999",
+                crm,
+                especialidade,
+                dadosEndereco()
+        );
+    }
+
+    private CadastroPacienteDTO dadosPaciente(String nome, String email, String cpf) {
+        return new CadastroPacienteDTO(
+                nome,
+                email,
+                "61999999999",
+                cpf,
+                dadosEndereco()
+        );
+    }
+
+    private DadosEnderecoDTO dadosEndereco() {
+        return new DadosEnderecoDTO(
+                "rua xpto",
+                "bairro",
+                "00000000",
+                "Brasilia",
+                "DF",
+                null,
+                null
+        );
     }
 }
