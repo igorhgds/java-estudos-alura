b05f3bef9c6ad2bca3f9e30be7cc987d7e25a19c
igorhgds <igorhenrique.gomes@hotmail.com>
2025-08-15 10:04:24 -0300

    criado configurações especificas de usuario

---
+++ spring-boot/spring-security2/src/main/resources/db/migration/V4__create-table-usuarios.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuarios
+(
+    id             BIGINT AUTO_IMCREMENT PRIMARY KEY,
+    email          VARCHAR(100) NOT NULL UNIQUE,
+    senha          VARCHAR(100) NOT NULL,
+    nome_completo  VARCHAR(100) NOT NULL,
+    nome_usuario   VARCHAR(100) NOT NULL UNIQUE,
+    mini_biografia VARCHAR(30),
+    biografia      TEXT
+);
